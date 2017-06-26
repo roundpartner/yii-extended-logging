e514421fd3d2621bbe6f6e094872b85435463783
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-06-26 08:48:00 +0100

    Test basic logger

--- tests/Unit/BasicLoggerTest.php
+++ tests/Unit/BasicLoggerTest.php
@@ -6,8 +6,33 @@ use RoundPartner\YiiLogger\BasicLogger;
 
 class BasicLoggerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var BasicLogger
+     */
+    protected $logRoute;
+
+    public function setUp()
+    {
+        $this->logRoute = new BasicLogger();
+        $this->logRoute->setLogPath('/tmp/');
+    }
+
     public function testCreateInstance()
     {
-        new BasicLogger();
+        $this->assertInstanceOf('\RoundPartner\YiiLogger\BasicLogger', $this->logRoute);
+    }
+
+    public function testBasicLoggerWithNoLog()
+    {
+        $logger = new \CLogger();
+        $this->logRoute->collectLogs($logger, true);
+    }
+
+    public function testBasicLoggerWithOneLog()
+    {
+        $logger = new \CLogger();
+        $logger->log('hello world');
+
+        $this->logRoute->collectLogs($logger, true);
     }
 }
